057971d9b9c121b7c996c05e580aae2707e4b50a
Mevlüt EFE <67636958+efemevlut@users.noreply.github.com>
2020-12-06 19:52:55 +0300

    Add findAll

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,12 @@
-exports.get_users = function (req, res, next) {
-  res.render("users");
+const UserModel = require("../models/User")
+exports.get_users = async (req, res, next) => {
+  try {
+    //get users from db
+    const userList = await UserModel.findAll();
+    console.log("user list", userList)
+    res.render("users", {userList});
+    
+  } catch (error) {
+    res.send("An error occured")
+  }
 };
